2a0f771f15a832dc3abfb24ae032679051a68eec
Jaymin Bhavsar <jayminbhavsar07@gmail.com>
2024-08-23 10:02:39 +0530

    Converted File into class component

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,8 +1,12 @@
-export default function File({ name }) {
-  return (
-    <div className="text-lg flex items-center gap-1 p-1">
-      <span className="material-symbols-outlined text-slate-400">draft</span>
-      {name}
-    </div>
-  );
+import { Component } from "react";
+
+export default class File extends Component {
+  render() {
+    return (
+      <div className="text-lg flex items-center gap-1 p-1">
+        <span className="material-symbols-outlined text-slate-400">draft</span>
+        {this.props.name}
+      </div>
+    );
+  }
 }
